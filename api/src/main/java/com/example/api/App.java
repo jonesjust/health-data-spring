cd8d6606e28300b8ad230be3b4d38674b324817f
jonesjust <86092532+jjones254@users.noreply.github.com>
2024-09-05 16:27:21 -0500

    remove HttpStatus and ReponseEntity imports

--- api/src/main/java/com/example/api/App.java
+++ api/src/main/java/com/example/api/App.java
@@ -16,8 +16,6 @@ import org.mitre.synthea.engine.Generator;
 import org.mitre.synthea.helpers.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
